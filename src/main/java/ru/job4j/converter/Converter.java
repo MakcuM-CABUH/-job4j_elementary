6e8f7b21ead20d15f8328c565d773df0c6b78081
MakcuM-CABUH <job4j@mail.ru>
2023-11-27 23:41:44 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,13 +3,11 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60;
-        return rsl;
+        return value / 60;
     }
 
     public static void main(String[] args) {
@@ -18,5 +16,17 @@ public class Converter {
 
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dollar.");
+
+        float inEu = 140;
+        float expectedEu = 2;
+        float outEu = Converter.rubleToEuro(inEu);
+        boolean passedEu = expectedEu == outEu;
+        System.out.println("140 rubles are 2 euro. Test result: " + passedEu);
+
+        float inDoll = 120;
+        float expectedDoll = 2;
+        float outDoll = Converter.rubleToDollar(inDoll);
+        boolean passedDoll = expectedDoll == outDoll;
+        System.out.println("120 rubles are 2 dollars. Test result: " + passedDoll);
     }
 }
