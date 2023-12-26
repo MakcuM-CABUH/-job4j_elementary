15606f3a0c883259e933bb4387b5156eea7fe7fb
MakcuM-CABUH <job4j@mail.ru>
2023-12-26 20:40:59 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/condition/Triangle.java
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -3,17 +3,14 @@ package ru.job4j.condition;
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
 
-        boolean result = ((ab + ac) > bc && (ac + bc) > ab && (ab + bc) > ac);
+        boolean result = ab + ac > bc && ac + bc > ab && ab + bc > ac;
         return result;
     }
 
     public static void main(String[] args) {
 
         boolean res = Triangle.exist(2.0, 2.0, 2.0);
-        System.out.println(Triangle.exist(res));
-    }
+        System.out.println(res);
 
-    private static boolean exist(boolean res) {
-        return res;
     }
 }
--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -23,3 +23,4 @@ class TriangleTest {
         assertThat(result).isFalse();
     }
 }
+//  формальная правка
