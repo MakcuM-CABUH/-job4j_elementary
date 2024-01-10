9b80b7537fdf6c9c7f393fed979be2f83d64977a
MakcuM-CABUH <job4j@mail.ru>
2024-01-10 22:28:59 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CounterTest {
+
+    @Test
+    void from0To10sum55() {
+        int start = 0;
+        int finish = 10;
+        int sum = Counter.sum(start, finish);
+        int expected = 55;
+        assertThat(sum).isEqualTo(expected);
+    }
+
+    @Test
+    void from3To8sum33() {
+        int start = 3;
+        int finish = 8;
+        int sum = Counter.sum(start, finish);
+        int expected = 33;
+        assertThat(sum).isEqualTo(expected);
+    }
+
+    @Test
+    void from1To1sum1() {
+        int start = 1;
+        int finish = 1;
+        int sum = Counter.sum(start, finish);
+        int expected = 1;
+        assertThat(sum).isEqualTo(expected);
+    }
+
+    @Test
+    void fromMinus1To10sum54() {
+        int start = -1;
+        int finish = 10;
+        int sum = Counter.sum(start, finish);
+        int expected = 54;
+        assertThat(sum).isEqualTo(expected);
+    }
+
+    @Test
+    void from3To1sum0() {
+        int start = 3;
+        int finish = 1;
+        int sum = Counter.sum(start, finish);
+        int expected = 0;
+        assertThat(sum).isEqualTo(expected);
+    }
+}
