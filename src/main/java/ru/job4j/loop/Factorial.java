191343fb93b6b09916ac21471fb2df986ad8747a
MakcuM-CABUH <job4j@mail.ru>
2024-01-13 12:03:32 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calculate(int number) {
         int result = 1;
-        for (int index = number; index >= 1; index--) {
+        for (int index = 1; index <= number; index++) {
             result *= index;
             if (number == 0) {
                 result = 1;
