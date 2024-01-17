750223385920eca06bd119b6bb3c7ad51dd9c146
MakcuM-CABUH <job4j@mail.ru>
2024-01-17 23:25:14 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -1,7 +1,5 @@
 package ru.job4j.loop;
 
-import ru.job4j.condition.SwitchWeek;
-
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 1;
