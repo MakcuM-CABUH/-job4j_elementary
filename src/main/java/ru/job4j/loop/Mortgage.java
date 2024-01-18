0f100ba03967d8823dd73378b34d5811ef1f3078
MakcuM-CABUH <job4j@mail.ru>
2024-01-18 22:36:52 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,11 +2,11 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
+        int year = 0;
 
-        while (amount + amount * percent / 100 > salary) {
+        while (amount > 0) {
             year++;
-            amount = amount + amount * percent / 100 - salary;
+            amount += amount * percent / 100 - salary;
         }
         return year;
     }
