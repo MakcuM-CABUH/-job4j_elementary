df7e3ed098ecfb0df2ebef3cbc3a40ed89fc0136
MakcuM-CABUH <job4j@mail.ru>
2024-01-25 22:48:12 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Vladimir Lenin";
+        names[2] = "Iosif Stalin";
+        names[3] = "Vladimir Putin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
