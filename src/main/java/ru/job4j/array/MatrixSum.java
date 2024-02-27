c4b2d153963a5c75f10ff7d793d9f1845f39769c
MakcuM-CABUH <job4j@mail.ru>
2024-02-27 14:41:21 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/array/MatrixSum.java
+++ src/main/java/ru/job4j/array/MatrixSum.java
@@ -5,8 +5,7 @@ public class MatrixSum {
         int result = 0;
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                result = result + array[row][cell];
-
+                result += array[row][cell];
             }
         }
         return result;
