ceb51abe3c7a46d887663ece7ee5b25df06b1f59
MakcuM-CABUH <job4j@mail.ru>
2024-03-17 21:51:26 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -5,20 +5,17 @@ public class TwoNumberSum {
 
         int i = 0;
         int j = array.length - 1;
+
         while (i < j) {
             int sum = array[i] + array[j];
             if (sum == target) {
-                array = new int[]{i, j};
-                break;
+                return new int[]{i, j};
             } else if (sum > target) {
                 j--;
             } else {
                 i++;
             }
-            if (i == j) {
-                return new int[0];
-            }
         }
-        return array;
+        return new int[0];
     }
 }
