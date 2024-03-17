c5c79d00734becce5f44b01d92ab596d8d9db91f
MakcuM-CABUH <job4j@mail.ru>
2024-03-17 22:27:48 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -6,20 +6,15 @@ public class Machine {
     public static int[] change(int money, int price) {
 
         int[] coins = {10, 5, 2, 1};
-        int size = 0;
         int[] result = new int[100];
-        int cash = money - price;
+        int size = 0;
 
-        for (int q : coins) {
-            while (q <= cash) {
-                cash = cash - q;
+        for (int coin : coins) {
+            while (coin <= (money - price)) {
+                money = money - coin;
                 size += 1;
-                int[] res = Arrays.copyOf(result, size);
-                res[res.length - 1] = q;
-                result = res;
-            }
-            if (cash == 0) {
-                break;
+                result = Arrays.copyOf(result, size);
+                result[result.length - 1] = coin;
             }
         }
         return Arrays.copyOf(result, size);
