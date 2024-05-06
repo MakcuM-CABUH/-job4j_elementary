ece84e42950dd97f7d11736062b9147d4dffb32a
MakcuM-CABUH <job4j@mail.ru>
2024-05-06 14:29:28 +0500

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -3,20 +3,22 @@ package ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
-        return rsl;
+        return calculateIdealWeight(height, 100);
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        return calculateIdealWeight(height, 110);
+    }
+
+    private static double calculateIdealWeight(short height, int correction) {
+        return (height - correction) * 1.15;
     }
 
     public static void main(String[] args) {
         short height = 187;
         double man = Fit.manWeight(height);
-        System.out.println("Man 187 is " + man + " kg.");
+        System.out.println(String.format("Man 187 is %.2f kg.", man));
         double woman = Fit.womanWeight(height);
-        System.out.println("Woman 187 is " + woman + " kg.");
+        System.out.println(String.format("Woman 187 is %.2f kg.", woman));
     }
 }
